10de827160f48b496bcd180282b5d41b0db049f9
MohamedKaid <mohamedkaid014@gmail.com>
2025-03-29 18:44:06 -0400

    added a ball class gave the ball movment added a bouce effect for the ball when it hits the top of the screen

---
+++ ball.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.xmove=10
+        self.ymove=10 
+        
+    
+    def move(self):
+        newx=self.xcor()+self.xmove
+        newy=self.ycor()+self.ymove
+        self.goto(newx,newy)
+        
+    def bounce(self):
+        self.ymove*= -1
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
-
+from ball import Ball
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,23 +10,37 @@ screen.title("PONG")
 screen.tracer(0)
 
 
-rPaddle= Paddle((350, 0))
-lPaddle= Paddle((-350, 0))
+rPaddle= Paddle((370, 0))
+lPaddle= Paddle((-370, 0))
+
+ball = Ball()
 
 
 
 
 screen.listen()
+
+# Right paddle movment
 screen.onkeypress(rPaddle.goUp, "Up")
 screen.onkeyrelease(rPaddle.stopUp, "Up")
 screen.onkeypress(rPaddle.goDown, "Down")
 screen.onkeyrelease(rPaddle.stopDown, "Down")
 
+# Left paddle movment
+screen.onkeypress(lPaddle.goUp, "w")
+screen.onkeyrelease(lPaddle.stopUp, "w")
+screen.onkeypress(lPaddle.goDown, "s")
+screen.onkeyrelease(lPaddle.stopDown, "s")
 
 
 
 game_is_on =True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    
+    if ball.ycor() > 280 or ball.ycor() <-280:
+        ball.bounce()
     
 screen.exitonclick()
